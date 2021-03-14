2258ed55def8fa1e5f12a3300c2e2c74582e9a94
PetrBogomolov <BogomoloffPD@yandex.ru>
2021-03-14 11:46:23 +0300

    3. Класс хранилища пользователей UserStorage [#485240]

--- src/main/java/ru/job4j/multithreading/synchronization/storage/UserStorage.java
+++ src/main/java/ru/job4j/multithreading/synchronization/storage/UserStorage.java
@@ -8,37 +8,23 @@ import java.util.*;
 public class UserStorage {
 
     @GuardedBy("this")
-    private final Set<User> storage = new TreeSet<>(Comparator.comparing(User::getId));
+    private final Map<Integer, User> storage = new HashMap<>();
 
     public synchronized boolean add(User user) {
-        return storage.add(user);
+        return storage.putIfAbsent(user.getId(), user) == null;
     }
 
     public synchronized boolean update(User user) {
-        if (delete(user)) {
-            add(user);
-            return true;
-        } else {
-            System.out.println("User was not found");
-        }
-        return false;
+        return storage.replace(user.getId(), user) != null;
     }
 
     public synchronized boolean delete(User user) {
-        return storage.remove(user);
+        return storage.remove(user.getId(), user);
     }
 
     public synchronized void transfer(int fromId, int toId, int amount) {
-        User payer = null;
-        User recipient = null;
-        for (User user : storage) {
-            if (user.getId() == fromId) {
-                payer = user;
-            }
-            if (user.getId() == toId) {
-                recipient = user;
-            }
-        }
+        User payer = storage.get(fromId);
+        User recipient = storage.get(toId);
         if (payer != null && recipient != null) {
             if (payer.getAmount() > amount) {
                 payer.setAmount(payer.getAmount() - amount);
@@ -49,16 +35,5 @@ public class UserStorage {
         } else {
             System.out.println("Payer and Recipient were not found");
         }
-    }
-
-    public static void main(String[] args) {
-        UserStorage storage = new UserStorage();
-        User payer = new User(1, 1000);
-        User recipient = new User(2, 1000);
-        storage.add(payer);
-        storage.add(recipient);
-        storage.storage.forEach(System.out::println);
-        storage.transfer(1, 2, 750);
-        storage.storage.forEach(System.out::println);
     }
 }
